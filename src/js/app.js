de65358b5c72cdb8003358f5d23dd8cf7b7bc3ea
Aleks Kivuls <alekskivuls@users.noreply.github.com>
2017-03-27 16:31:06 -0700

    Chain UI Menus

--- src/js/app.js
+++ src/js/app.js
@@ -1,65 +1,78 @@
 var UI = require('ui');
 
 var main = new UI.Card({
-  title: 'Project',
-  subtitle: 'Tag',
+    title: 'Project',
+    subtitle: 'Tag',
     body: 'Time Started: \n10/10/10 \n10:10:10',
-  subtitleColor: 'indigo', // Named colors
-  bodyColor: '#9a0036' // Hex colors
+    subtitleColor: 'indigo', // Named colors
+    bodyColor: '#9a0036' // Hex colors
 });
 
 main.show();
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
+
+var workspaceMenu = new UI.Menu({
     sections: [{
         title: 'Workspaces',
-      items: [{
-        title: 'Workspace One',
-      }, {
-        title: 'Workspace Two',
-      }]
+        items: [{
+            title: 'Workspace One',
+        }, {
+            title: 'Workspace Two',
+        }]
     }]
-  });
-  menu.on('select', function(e) {
+});
+
+var projectMenu = new UI.Menu({
+    sections: [{
+        title: 'Projects',
+        items: [{
+            title: 'Project One',
+        }, {
+            title: 'Project Two',
+        }]
+    }]
+});
+
+var tagMenu = new UI.Menu({
+    sections: [{
+        title: 'Tags',
+        items: [{
+            title: 'Tag One',
+        }, {
+            title: 'Tag Two',
+        }]
+    }]
+});
+
+workspaceMenu.on('select', function(e) {
     console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
     console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
+    projectMenu.show();
+    workspaceMenu.hide();
+});
+
+projectMenu.on('select', function(e) {
+    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+    console.log('The item is titled "' + e.item.title + '"');
+    tagMenu.show();
+    projectMenu.hide();
+});
+
+tagMenu.on('select', function(e) {
+    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+    console.log('The item is titled "' + e.item.title + '"');
+    main.show();
+    tagMenu.hide();
+});
+
+main.on('click', 'up', function(e) {
+    workspaceMenu.show();
 });
 
 main.on('click', 'select', function(e) {
-   var menu = new UI.Menu({
-    sections: [{
-        title: 'Projects',
-      items: [{
-        title: 'Project One',
-      }, {
-        title: 'Project Two',
-      }]
-    }]
-  });
-    menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
+    projectMenu.show();
 });
 
 main.on('click', 'down', function(e) {
- var menu = new UI.Menu({
-    sections: [{
-        title: 'Tags',
-      items: [{
-        title: 'Tag One',
-      }, {
-        title: 'Tag Two',
-      }]
-    }]
-  });
-    menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
+    tagMenu.show();
 });
